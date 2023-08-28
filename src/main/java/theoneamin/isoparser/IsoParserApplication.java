2ff35e6ae504252c3f17d5bbae907f448a13e595
Amin <abdimamin@gmail.com>
2023-08-28 22:23:36 +0100

    setup project

---
+++ src/main/java/theoneamin/isoparser/IsoParserApplication.java
@@ -0,0 +1,13 @@
+package theoneamin.isoparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsoParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsoParserApplication.class, args);
+	}
+
+}
